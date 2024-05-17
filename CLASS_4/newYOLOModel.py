33e00b11008704dd7c8e16da209fac127bd5501c
shahiduljahid <shahiduljahid3@gmail.com>
2024-05-17 15:59:25 +0800

    new train data updated

--- CLASS_4/newYOLOModel.py
+++ CLASS_4/newYOLOModel.py
@@ -2,12 +2,12 @@ from ultralytics import YOLO
 
 model1 = YOLO('yolov8n.yaml').load('yolov8n.pt') # build from YAML and transfer weights
 #DATASET_PATH
-PATH1 = '/Users/macbook/Desktop/python_practical/pythonpa/dataset/drone.yaml'
+PATH1 = r'C:\Users\ADMIN\Desktop\Python_practical\pythonpa\dataset\drone.yaml'
 # Train the model
-results = model1.train(data=PATH1, epochs=10, imgsz=640 ,batch=1)
+results = model1.train(data=PATH1, epochs=20, imgsz=1000 ,batch=2)
 
 model2 = YOLO('yolov8n.yaml').load('yolov8n.pt') # build from YAML and transfer weights
 #DATASET_PATH
-PATH2 = '/Users/macbook/Desktop/python_practical/CLASS_4/drone.yaml'
+PATH2 = r'C:\Users\ADMIN\Desktop\Python_practical\CLASS_4\drone.yaml'
 # Train the model
-results = model2.train(data=PATH2, epochs=10, imgsz=640 ,batch=1)
+results = model2.train(data=PATH2, epochs=20, imgsz=1000 ,batch=2)
